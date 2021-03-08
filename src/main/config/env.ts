404195b72e307670c707238294206f75747b69c5
Leandro0101 <leandrovieira3306@gmail.com>
2021-03-08 19:09:28 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://leand:leand123@cluster0.hn9en.mongodb.net/clean_mango?retryWrites=true&w=majority',
   port: process.env.PORT || 5050
 }
