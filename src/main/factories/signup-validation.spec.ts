e66c601dcf5ff9767951381d9b41a604d60f457e
leandro0101 <leandrovieira3306@gmail.com>
2021-05-10 10:13:18 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { RequiredFieldValidation } from '../../presentation/helpers/validators/r
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { makeSignUpValidation } from './signup-validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
 
@@ -13,6 +14,7 @@ describe('SignUpValidation factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
---
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '../../errors'
+import { Validation } from './validation'
+
+export class CompareFieldsValidation implements Validation {
+  private readonly fieldname: string
+  private readonly fieldToComparename: string
+  constructor (fieldname: string, fieldToComparename: string) {
+    this.fieldname = fieldname
+    this.fieldToComparename = fieldToComparename
+  }
+
+  validate (input: any): Error {
+    if (input[this.fieldname] !== input[this.fieldToComparename]) {
+      return new InvalidParamError(this.fieldToComparename)
+    }
+  }
+}
