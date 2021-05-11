24a8c705f008dc585be8dd0ff3f9e5383740b3d2
leandro0101 <leandrovieira3306@gmail.com>
2021-05-11 11:52:31 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -52,4 +52,10 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@gmail.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@gmail.com')
+    expect(account).toBeFalsy()
+  })
 })
