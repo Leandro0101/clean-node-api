3ce1fefaf13f6a2312b7d882231387469ca4c319
leandro0101 <leandrovieira3306@gmail.com>
2021-05-11 15:03:53 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://leand:leand123@cluster0.hn9en.mongodb.net/clean_mango?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'l20d/~1'
 }
