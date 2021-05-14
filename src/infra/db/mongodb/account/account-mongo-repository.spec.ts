896fb40e47fdaebf3d1a82ee37a29fc17a6e22cf
leandro0101 <leandrovieira3306@gmail.com>
2021-05-14 11:28:17 -0300

    fix: ensure AccountMongoRepository allows admin to access any route

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -98,17 +98,48 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
   })
-  test('Should return an account on loadByToken with role', async () => {
+  test('Should return an account on loadByToken with admin role', async () => {
     const sut = makeSut()
     await accountCollection.insertOne({
       name: 'any_name',
       email: 'any_email@gmail.com',
       password: 'any_password',
       accessToken: 'any_token',
-      role: 'any_role'
+      role: 'admin'
     })
 
-    const account = await sut.loadByToken('any_token', 'any_role')
+    const account = await sut.loadByToken('any_token', 'admin')
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@gmail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('Should return null on loadByToken with invalid role', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password',
+      accessToken: 'any_token'
+    })
+
+    const account = await sut.loadByToken('any_token', 'admin')
+    expect(account).toBeFalsy()
+  })
+
+  test('Should return an account on loadByToken with if user is admin', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password',
+      accessToken: 'any_token',
+      role: 'admin'
+    })
+
+    const account = await sut.loadByToken('any_token')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
