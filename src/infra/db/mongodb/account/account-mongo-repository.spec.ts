e154c11844933143abc7ccb3bfb116a8a7b0c2e3
leandro0101 <leandrovieira3306@gmail.com>
2021-05-14 10:30:47 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -115,4 +115,10 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@gmail.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should return null if loadByToken fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByToken('any_token')
+    expect(account).toBeFalsy()
+  })
 })
